5eaf5fe0d51d392f0f78825f99d7073f9e4d399a
Fooxii <ironata07@gmail.com>
2026-01-18 19:56:37 -0400

    Added python script that prints a string in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= 65 and ord(c) <= 90:
-        return True
-    return False
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
