8eae22f7df0d33483ec9e7f2841ef77917c4deac
Fooxii <ironata07@gmail.com>
2026-01-18 21:35:42 -0400

    removed signed functionality

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     lastdig = abs(number) % 10
-    if number < 0:
-        print(f"-{lastdig}",end="")
+    print(f"{lastdig}",end="")
     return lastdig
