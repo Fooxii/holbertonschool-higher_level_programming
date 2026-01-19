2dd9da55c9e61e6f42de30d004a4dcf0d1792d0a
Fooxii <ironata07@gmail.com>
2026-01-18 20:17:34 -0400

    Limited print function usage to only 1 by adding a variable that handles the new string and printing the new string at the end of script

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
+
     for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
+        if 97 <= ord(c) <= 122:
+            result += chr(ord(c) - 32)
         else:
-            print("{}".format(c), end="")
+            result += c
+
+    print("{}".format(result))
