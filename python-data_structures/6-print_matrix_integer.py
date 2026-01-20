8239beae916fc578a1fb17c78b69dfee554e5bbd
Fooxii <ironata07@gmail.com>
2026-01-20 17:05:47 -0400

    Fixed script and correctly used .format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    i = 0
-    while i < len(matrix):
-        print("{:d}".format(matrix[i]))
-        i += 1
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[i][j]), end="")
+        print()
