38bd69f190d534a8198c6e0c7d315bbb96d79f06
Fooxii <ironata07@gmail.com>
2026-01-21 22:21:03 -0400

    Forgot to define the initial values of two variables being used

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman_num = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
+    total = 0
+    prev_num = 0
     for c in roman_string:
         if c in roman_num:
             total += roman_num[c]
