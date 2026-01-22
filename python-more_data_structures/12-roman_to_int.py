0a59be96928f475c2bd210bd031cfdab4d5181d4
Fooxii <ironata07@gmail.com>
2026-01-21 22:32:34 -0400

    Fixed return values and added guard if statement for when roman_string is not a string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     roman_num = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
     total = 0
     prev_num = 0
@@ -10,5 +12,5 @@ def roman_to_int(roman_string):
                 total -= prev_num * 2
             prev_num = roman_num[c]
         else:
-            return None
+            return 0
     return total
