d04533444eff4680659ca242b3ecb5f4ec6a4a4a
Fooxii <ironata07@gmail.com>
2026-01-29 17:54:54 -0400

    Added python script that writes a class that defines a square and prints a square the size of size

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Docstring for my_print
+        """
+        for i in self.__size:
+            for j in self.__size:
+                print("#", end="")
