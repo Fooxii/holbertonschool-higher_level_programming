1b09305d25f47b9a64c9f3210970b73caa124471
Fooxii <ironata07@gmail.com>
2026-02-01 00:27:45 -0400

    Accidentally wrote over an existing file

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -43,17 +43,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-
-        rect = ""
-        for i in range(self.__height):
-            for _ in range(self.__width):
-                rect += "#"
-
-            if i != self.__height - 1:
-                rect += "\n"
-
-        return rect
