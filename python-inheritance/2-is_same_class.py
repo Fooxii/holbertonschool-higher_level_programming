8187ca6979775f7440ed4d398d65e61fefd3955a
Fooxii <ironata07@gmail.com>
2026-02-02 20:14:43 -0400

    pycodestyle fixes

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,11 +7,11 @@ Docstring for python-inheritance.2-is_same_class
 def is_same_class(obj, a_class):
     """
     Docstring for is_same_class
-    
+
     :param obj: Description
     :param a_class: Description
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
