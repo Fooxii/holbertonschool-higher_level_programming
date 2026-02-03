2554456061ea6dd5354cb2da81cf49196dc6a99a
Fooxii <ironata07@gmail.com>
2026-02-02 20:31:28 -0400

    Added python script that returns True if the object is an instance of or a subclass of the specified class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Docstring for python-inheritance.3-is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Docstring for is_kind_of_class
+
+    :param obj: Description
+    :param a_class: Description
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
