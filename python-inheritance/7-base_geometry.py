553927322ef7cf4eb36eeccc2b6b68d6ecdb0d76
Fooxii <ironata07@gmail.com>
2026-02-03 11:18:18 -0400

    Added python script that writes a class called BaseGeometry that raises an error if value is not an integer and if value is less or equal to 0

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Docstring for python-inheritance.6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Docstring for BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
