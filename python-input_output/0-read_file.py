3ab37c02e0f7f9f9499c52a82e13cf7cabc911c4
Fooxii <ironata07@gmail.com>
2026-02-11 14:51:26 -0400

    pycodestyle fixes

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,10 +3,11 @@
 Docstring for python-input_output.0-read_file
 """
 
+
 def read_file(filename=""):
     """
     Docstring for read_file
-    
+
     :param filename: Description
     """
     with open(filename, 'r', encoding="utf-8") as file:
