084b61d5f3c8d0c4c812b8e3d140a4801c8bc369
Fooxii <ironata07@gmail.com>
2026-02-11 22:28:25 -0400

    Add from_json_string function to deserialize JSON strings into Python objects

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.4-from_json_string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Docstring for from_json_string
+
+    :param my_str: Description
+    """
+    pds = json.loads(my_str)
+    return pds
