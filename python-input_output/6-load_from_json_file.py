926bf838915d1bbd578c0f8a3e402fdc0d978bfb
Fooxii <ironata07@gmail.com>
2026-02-11 22:57:00 -0400

    Add load_from_json_file function to deserialize JSON files into Python objects

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.6-load_from_json_file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Docstring for load_from_json_file
+
+    :param filename: Description
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        my_obj = json.load(file)
+        return my_obj
