308322280b3d10bb10fde9acc44c99e29a72de4c
Fooxii <ironata07@gmail.com>
2026-02-15 16:01:58 -0400

    Add Student class with initialization and JSON serialization methods

---
+++ python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Docstring for python-input_output.11-student
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Docstring for __init__
+
+        :param self: Description
+        :param first_name: Description
+        :param last_name: Description
+        :param age: Description
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Docstring for to_json
+
+        :param self: Description
+        :param attrs: Description
+        """
+        if isinstance(attrs, list):
+            new_dict = {}
+            for attr in attrs:
+                if isinstance(attr, str) and attr in self.__dict__:
+                    new_dict[attr] = self.__dict__[attr]
+            return new_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Docstring for reload_from_json
+
+        :param self: Description
+        :param json: Description
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
