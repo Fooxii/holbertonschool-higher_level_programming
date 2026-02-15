7ae17da892a6ed9f3847ee5522350c89955fbdaa
Fooxii <ironata07@gmail.com>
2026-02-15 13:49:35 -0400

    Fix import statements in 7-add_item.py for clarity and consistency

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 Docstring for python-input_output.7-add_item
 """
 import sys
-import save_to_json_file from 5-save_to_json_file
-import load_from_json_file from 6-load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 file = "add_item.json"
 
