f188cde0de4353f945dd79389cdb3d051e8e430e
Fooxii <ironata07@gmail.com>
2026-02-15 14:51:00 -0400

    Fix attribute assignment in Student class constructor

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -14,9 +14,9 @@ class Student:
         :param last_name: Description
         :param age: Description
         """
-        first_name = first_name
-        last_name = last_name
-        age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """
