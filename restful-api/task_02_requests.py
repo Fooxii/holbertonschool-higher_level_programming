a852184f64849459d08c22115bff48eaf90b0ddf
Fooxii <ironata07@gmail.com>
2026-02-22 10:15:04 -0400

    Added README file and task_02

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+def fetch_and_print_posts():
+    response = requests.get(URL)
+
+    print("Status Code: {}".format(response.status_code))
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts:
+            print(post["title"])
+
+
+def fetch_and_save_posts():
+    response = requests.get(URL)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+    structured_posts = []
+
+    for post in posts:
+        structured_posts.append({
+            "id": post["id"],
+            "title": post["title"],
+            "body": post["body"]
+        })
+
+    with open("posts.csv", 'w', newline="", encoding="UTF-8") as file:
+        writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+
+        writer.writeheader()
+        writer.writerows(structured_posts)
+
+
+if __name__ == "__main__":
+    fetch_and_print_posts
+    fetch_and_save_posts
