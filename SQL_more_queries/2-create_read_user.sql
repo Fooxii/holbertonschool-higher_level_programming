537c7419decc92974235abbca6b0f849047a8177
Fooxii <ironata07@gmail.com>
2026-03-05 13:33:30 -0400

    Add SQL script to create a database and user with SELECT privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- This script will create a database and user if they don't already exist, the user will be given a set password and will have only SELECT privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_02_2'@'localhost';
