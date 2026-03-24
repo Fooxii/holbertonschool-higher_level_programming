1bb46352e4f1edefa38c363f8e75473eefa28ece
Fooxii <ironata07@gmail.com>
2026-03-23 23:09:25 -0400

    semistandard fixes

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let n = 0;
 while (n < array.length) {
   console.log(array[n]);
